e9afef505ce9faa0b938434f3bbc762a37083e2d
Danny Ben Shitrit <dannyb@telesofia.com>
2018-02-15 16:49:57 +0000

    updates

--- lib/rigit/command_line.rb
+++ lib/rigit/command_line.rb
@@ -2,30 +2,25 @@ require 'super_docopt'
 
 module Rigit
   class CommandLine < SuperDocopt::Base
-    version '0.0.0'
+    version VERSION
     docopt File.expand_path 'docopt', __dir__
     subcommands [:build]
-
-    attr_reader :config
 
     def build
       source = args['RIG']
       name   = args['NAME']
-      @config = config_for source
+      config = config_for source
+
+      prompt = Prompt.new config.params, intro: config.intro
+      arguments = prompt.get_input params
+
+      arguments[:name] = params[:name] || args['NAME']
+
       rigger = Rigger.new source, name, arguments
       rigger.scaffold
     end
 
     private
-
-    def arguments
-      result = {}
-      config.params.each do |key, spec|
-        result[key.to_sym] = params[key] || spec.default
-      end
-      result[:name] = params[:name] || args['NAME']
-      result
-    end
 
     def params
       @params ||= params!
@@ -39,6 +34,10 @@ module Rigit
         output[key.to_sym] = value
       end
       output
+    end
+
+    def prompt
+      @prompt ||= Prompt.new
     end
 
     def config_for(source)
---
+++ lib/rigit/commands/build.rb
@@ -0,0 +1,48 @@
+require 'super_docopt'
+
+module Rigit::Commands
+  module Build
+    def build
+      source = args['RIG']
+      name   = args['NAME']
+      config = config_for source
+
+      prompt = Prompt.new config.params, intro: config.intro
+      arguments = prompt.get_input params
+
+      arguments[:name] = params[:name] || args['NAME']
+
+      rigger = Rigger.new source, name, arguments
+      rigger.scaffold
+    end
+
+    class Builder
+
+      def initizlize
+        # ...
+      end
+
+      def params
+        @params ||= params!
+      end
+
+      def params!
+        output = {}
+        input = args['PARAMS']
+        input.each do |param|
+          key, value = param.split '='
+          output[key.to_sym] = value
+        end
+        output
+      end
+
+      def prompt
+        @prompt ||= Prompt.new
+      end
+
+      def config_for(source)
+        Config.load("#{source}/config.yml")
+      end
+    end
+  end
+end
