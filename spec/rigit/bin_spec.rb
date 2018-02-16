b919fe27ce76b7df206512a84480e0d054b11a98
Danny Ben Shitrit <dannyb@telesofia.com>
2018-02-16 20:40:33 +0000

    complete bin spec, closes #2

--- spec/rigit/bin_spec.rb
+++ spec/rigit/bin_spec.rb
@@ -2,22 +2,39 @@ require 'spec_helper'
 
 describe 'bin/rig' do
   context "without arguments" do
-    it "shows usage"
+    it "shows usage" do
+      expect(`bin/rig`).to match_fixture('cli/usage')
+    end
   end
 
   describe 'build' do
-    it "asks for input and builds a rig"
+    let(:workdir) { 'spec/tmp' }
+    before { reset_workdir }
+
+    it "builds a rig" do
+      Dir.chdir workdir do
+        expect(`../../bin/rig build minimal`).to match_fixture('cli/build-nocolor')
+      end
+    end
 
     context "when source rig does not exist" do
-      it "exits with grace"
+      it "exits with grace" do
+        expect(`bin/rig build norig`).to match_fixture('cli/build-norig')
+      end
     end
 
     context "with an invalid rig config" do
-      it "exits with grace"
-    end
+      before do
+        system 'rm -rf examples/broken' if Dir.exist? 'examples/bdoken'
+        system 'cp -R examples/minimal examples/broken'
+        system 'cp spec/fixtures/prompt-invalid.yml examples/broken/config.yml'
+      end
 
-    context "with input is interrupted" do
-      it "exits with grace"
+      it "exits with grace" do
+        Dir.chdir workdir do
+          expect(`../../bin/rig build broken`).to match_fixture('cli/build-broken')
+        end
+      end
     end
   end
 
