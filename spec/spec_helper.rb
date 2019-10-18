bf3302691a548eb7362793859ee525ab2c70753b
Danny Ben Shitrit <db@dannyben.com>
2019-10-18 19:24:16 +0000

    strip escape chars from rspec fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,8 +12,9 @@ require_relative '../lib/rigit'
 include Rigit
 require_relative 'spec_mixin'
 
-RSpec.configure do |c|
-  c.include SpecMixin
+RSpec.configure do |config|
+  config.include SpecMixin
+  config.strip_ansi_escape = true
 end
 
 Rig.home = "#{Dir.pwd}/spec/rigs"
